4a7931722662283cb538030c69dc3eab92ac421e
이가현 <igh2468@likelion.org>
2023-03-30 00:16:43 +0900

    스마트컨트랙트 구현 완성

--- naegginiggi/donation_point/views.py
+++ naegginiggi/donation_point/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,66 +8,8 @@ from django.utils import timezone
 from knox.auth import TokenAuthentication
 
 from user_custom.models import User_Custom
-from campaign.models import User_Campaign
+from campaign.models import User_Campaign, Campaign
 # Create your views here.
-
-# def first(request):
-#     return render(request, 'kakaopay_redirectPage.html')
-
-
-# @api_view(['POST'])
-# def index(request):
-#         URL = 'https://kapi.kakao.com/v1/payment/ready'3
-#         headers = {
-#             "Authorization": "KakaoAK " + "d67108a72dca85785839d343f6075928",   # 변경불가
-#             "Content-type": "application/x-www-form-urlencoded;charset=utf-8",  # 변경불가
-#         }
-#         params = {
-#             "cid": "TC0ONETIME",    # 테스트용 코드
-#             "partner_order_id": "1001",     # 주문번호
-#             "partner_user_id": "admin",    # 유저 아이디
-#             "item_name": "기부 포인트 1000 meal 결재",        # 구매 물품 이름
-#             "quantity": "1000",                # 구매 물품 수량
-#             "total_amount": "1000",        # 구매 물품 가격
-#             "tax_free_amount": "0",         # 구매 물품 비과세
-#             "approval_url": "http://127.0.0.1:8000/donation/approval/",
-#             "cancel_url": "http://127.0.0.1:8000/donation/",
-#             "fail_url": "http://127.0.0.1:8000/donation/",
-#         }
-
-#         res = requests.post(URL, headers=headers, params=params)
-#         request.session['tid'] = res.json()['tid']      # 결제 승인시 사용할 tid를 세션에 저장
-#         next_url = res.json()['next_redirect_pc_url']   # 결제 페이지로 넘어갈 url을 저장
-#         return redirect(next_url)
-
-#     return render('kakaopay_redirectPage.html')
-
-
-# def approval(request):
-#     URL = 'https://kapi.kakao.com/v1/payment/approve'
-#     headers = {
-#         "Authorization": "KakaoAK " + "자신의 admin key 넣기",
-#         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-#     }
-#     params = {
-#         "cid": "TC0ONETIME",    # 테스트용 코드
-#         "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
-#         "partner_order_id": "1001",     # 주문번호
-#         "partner_user_id": "german",    # 유저 아이디
-#         "pg_token": request.GET.get("pg_token"),     # 쿼리 스트링으로 받은 pg토큰
-#     }
-
-#     res = requests.post(URL, headers=headers, params=params)
-#     amount = res.json()['amount']['total']
-#     res = res.json()
-#     context = {
-#         'res': res,
-#         'amount': amount,
-#     }
-#     return render(request, 'approval.html', context)
-######### 카카오 페이 결재 완료된 후에 캠페인 아이디 넘겨주기 !!
-
-
 def define_user(request):
     token = request.META.get('HTTP_AUTHORIZATION', False)
     token = str(token).split()[1].encode("utf-8")
@@ -76,6 +18,70 @@ def define_user(request):
     return user
 
 
+def approve(request):
+    render('approve.html')
+    
+    
+# 유저, 기부금액, 캠페인아이디 필요
+@api_view(['POST'])
+def index(request):
+    user = define_user(request)
+    campaign = get_object_or_404(Campaign, campaign_id=request.data['campaign_id'])
+    donation = request.data['donation']
+    URL = 'https://kapi.kakao.com/v1/payment/ready'
+    headers = {
+        "Authorization": "KakaoAK " + "d67108a72dca85785839d343f6075928",   # 변경불가
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",  # 변경불가
+    }
+    params = {
+        "cid": "TC0ONETIME",    # 테스트용 코드
+        "partner_order_id": str(campaign.campaign_id),     # 주문번호
+        "partner_user_id": str(user.username),    # 유저 아이디
+        "item_name": str(campaign.title),        # 구매 물품 이름
+        "quantity": "1",                # 구매 물품 수량
+        "total_amount": str(donation),        # 구매 물품 가격
+        "tax_free_amount": "0",         # 구매 물품 비과세
+        "approval_url": "http://127.0.0.1:8000/donation/approve/",
+        "cancel_url": "http://127.0.0.1:8000/donation/cancel/",
+        "fail_url": "http://127.0.0.1:8000/donation/fail/",
+    }
+
+    res = requests.post(URL, headers=headers, params=params)
+    # 결제 승인시 사용할 tid를 세션에 저장
+    # next_url = res.json()['next_redirect_pc_url']   # 결제 페이지로 넘어갈 url을 저장
+    
+    return Response(res,status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def approval(request):
+    user = define_user(request)
+    campaign = get_object_or_404(Campaign, id=request.data['campaign_id'])
+    URL = 'https://kapi.kakao.com/v1/payment/approve'
+    headers = {
+        "Authorization": "KakaoAK " + "d67108a72dca85785839d343f6075928",
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+    params = {
+        "cid": "TC0ONETIME",    # 테스트용 코드
+        "tid": request.data['tid'],  # 결제 요청시 세션에 저장한 tid
+        "partner_order_id": str(campaign.campaign_id),     # 주문번호
+        "partner_user_id": str(user.username),    # 유저 아이디
+        "pg_token": request.GET.get("pg_token"),     # 쿼리 스트링으로 받은 pg토큰
+    }
+
+    res = requests.post(URL, headers=headers, params=params)
+    amount = res.json()['amount']['total']
+    res = res.json()
+    context = {
+        'res': res,
+        'amount': amount,
+    }
+    return Response(context, status=status.HTTP_200_OK)
+
+
+# 카카오 페이 결재 완료된 후에 캠페인 아이디랑 결제한 포인트 넘겨주기 !!
+
 @api_view(['POST'])
 def accumulatePointByUserId(request):
     user = define_user(request)
@@ -83,22 +89,24 @@ def accumulatePointByUserId(request):
     point_amount = request.data['point']
     campaign_id = request.data['campaign_id']
     campaign = get_object_or_404(User_Campaign, id=campaign_id)
-    user_custom = get_object_or_404(User_Custom, user = user)
-    description = str(timezone.now()) + " "+ str(user.username)+" "+ str(point_amount) + " 포인트 충전"
-    if User_Campaign.objects.filter(user=user,campaign=campaign).exists():
-        User_Campaign.objects.get(user=user, campaign=campaign).donation_amount += point_amount
+    user_custom = get_object_or_404(User_Custom, user=user)
+    description = str(timezone.now()) + " " + \
+        str(user.username)+" " + str(point_amount) + " 포인트 충전"
+    if User_Campaign.objects.filter(user=user, campaign=campaign).exists():
+        User_Campaign.objects.get(
+            user=user, campaign=campaign).donation_amount += point_amount
     else:
         User_Campaign.objects.create(
             user=user,
             campaign=campaign,
-            donation_amount = point_amount
+            donation_amount=point_amount
         )
     payload = {
-        "orderIdentifier" : str(user.username)+"USER_add "+str(user_custom.donation_count+1)+"to Campaign "+str(campaign_id),
-        "userIdentifier" : str(user.password),
-        "loyaltyProgramId" : "1564707676167177217",
-        "amount" : str(point_amount),
-        "description" : description,
+        "orderIdentifier": str(user.username)+"USER_add "+str(user_custom.donation_count+1)+"to Campaign "+str(campaign_id),
+        "userIdentifier": str(user.password),
+        "loyaltyProgramId": "1564707676167177217",
+        "amount": str(point_amount),
+        "description": description,
     }
 
     headers = {
@@ -107,9 +115,6 @@ def accumulatePointByUserId(request):
         "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ2ZXIiOiJ2MSIsInRrbiI6ImI4ZjgzY2NkZGIyZTU5MjkiLCJ0cGUiOiJJQU0iLCJzbHQiOiJlYzQyZjkwMTJjMjFjYTA3IiwiaWF0IjoxNjc2NTI2MTQxLCJleHAiOjE2NzcxMzA5NDEsImlzcyI6Imx1bnZzOmJhYXM6YXV0aDpzZXJ2aWNlIn0.8b4rysCN1x5x88YjAWhdnivVAJc6o7ReKuUUCzEyEGX9pD0ASOewdvGPHwyHHVIjAf-x-U15pkyk67N09JNisQ"
     }
     response = requests.post(url, json=payload, headers=headers)
-    User_Campaign.objects.create(
-        
-    )
     return Response(response, status=status.HTTP_200_OK)
 
 
@@ -118,19 +123,20 @@ def redeemPointByUserId(request):
     user = define_user(request)
     url = "https://api.luniverse.io/svc/v2/mercury/point/spend"
     point_amount = request.data['point']
-    user_custom = get_object_or_404(User_Custom, user = user)
+    user_custom = get_object_or_404(User_Custom, user=user)
     campaign_id = request.data['campaign_id']
     campaign = get_object_or_404(User_Campaign, id=campaign_id)
-    User_Campaign.objects.get(user=user, campaign=campaign).donation_amount -= point_amount
-    
+    User_Campaign.objects.get(
+        user=user, campaign=campaign).donation_amount -= point_amount
+
     payload = {
-        "orderIdentifier" : str(user.username)+"USER_redeem "+str(user_custom.donation_count+1)+"to Campaign "+str(campaign_id),
-        "userIdentifier" : str(user.password),
-        "loyaltyProgramId" : "1564707676167177217",
-        "amount" : str(point_amount),
-        "description" : str(timezone.now()) + " "+ str(user.username)+" "+ str(point_amount) + " 포인트 차감"
+        "orderIdentifier": str(user.username)+"USER_redeem "+str(user_custom.donation_count+1)+"to Campaign "+str(campaign_id),
+        "userIdentifier": str(user.password),
+        "loyaltyProgramId": "1564707676167177217",
+        "amount": str(point_amount),
+        "description": str(timezone.now()) + " " + str(user.username)+" " + str(point_amount) + " 포인트 차감"
     }
-    
+
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
@@ -144,7 +150,8 @@ def redeemPointByUserId(request):
 @api_view(['GET'])
 def getUserBalance(request):
     user = define_user(request)
-    url = "https://api.luniverse.io/svc/v2/mercury/point-accounts/"+str(user.password)+"/balances/MEAL"
+    url = "https://api.luniverse.io/svc/v2/mercury/point-accounts/" + \
+        str(user.password)+"/balances/MEAL"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ2ZXIiOiJ2MSIsInRrbiI6ImI4ZjgzY2NkZGIyZTU5MjkiLCJ0cGUiOiJJQU0iLCJzbHQiOiJlYzQyZjkwMTJjMjFjYTA3IiwiaWF0IjoxNjc2NTI2MTQxLCJleHAiOjE2NzcxMzA5NDEsImlzcyI6Imx1bnZzOmJhYXM6YXV0aDpzZXJ2aWNlIn0.8b4rysCN1x5x88YjAWhdnivVAJc6o7ReKuUUCzEyEGX9pD0ASOewdvGPHwyHHVIjAf-x-U15pkyk67N09JNisQ"
@@ -152,10 +159,12 @@ def getUserBalance(request):
     response = requests.get(url, headers=headers)
     return Response(response, status=status.HTTP_200_OK)
 
+
 @api_view(['GET'])
 def listPointHistories(request):
     user = define_user(request)
-    url = "https://api.luniverse.io/svc/v2/mercury/point/histories?userIdentifier="+str(user.password)+"&loyaltyProgramId=1564707676167177217&rpp=50&page=1"
+    url = "https://api.luniverse.io/svc/v2/mercury/point/histories?userIdentifier=" + \
+        str(user.password)+"&loyaltyProgramId=1564707676167177217&rpp=50&page=1"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ2ZXIiOiJ2MSIsInRrbiI6ImI4ZjgzY2NkZGIyZTU5MjkiLCJ0cGUiOiJJQU0iLCJzbHQiOiJlYzQyZjkwMTJjMjFjYTA3IiwiaWF0IjoxNjc2NTI2MTQxLCJleHAiOjE2NzcxMzA5NDEsImlzcyI6Imx1bnZzOmJhYXM6YXV0aDpzZXJ2aWNlIn0.8b4rysCN1x5x88YjAWhdnivVAJc6o7ReKuUUCzEyEGX9pD0ASOewdvGPHwyHHVIjAf-x-U15pkyk67N09JNisQ"
--- naegginiggi/test.sol
+++ naegginiggi/test.sol
@@ -1,20 +1,84 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
-contract test {
+contract NaeggiDonation {
     // 초기 비즈니스 로직
+    // safe remote purchase와 비슷
     // 기부자 측에서 캠페인 모금금고에 입금 시, 트렌젝션 2번 발생 (기부자 포인트 차감, 나눔단체 포인트 증가)
     // 기부자, 나눔단체 사이에는 포인트로만 거래함 -> 우리(기부 중계 업체인 회사)는 포인트를 회수(redeem)할 때만 원화로 정산해주면 되는 것임. 
-    address public hlogName;
+    address payable public hlogName;
     address public donator;
     uint public donation_amount;
-    address public owner;
+    enum State { Created, Locked, Inactive }
+    State public state;
 
-    event Donation(address donator, uint donation_amount);
-
-    constructor () {
-        owner = msg.sender;
+    constructor() payable {
+        donator = msg.sender;
+        donation_amount = msg.value;
     }
 
-    function bid() public
+    event Finished();
+    event Donation();
+    event DonationReceived();
+
+    modifier onlyDonator() {
+        require(msg.sender == donator);
+        _;
+    }
+    modifier onlyFoundation() {
+        require(msg.sender == hlogName);
+        _;
+    }
+    modifier inState(State _state) {
+        require(state == _state);
+        _;
+    }
+
+    /// 구매를 중단하고 이더를 회수합니다.
+    /// 콘트렉트가 잠기기 전에 판매자에 의해서만
+    /// 호출 되어 질 수 있습니다.
+    function abort()
+        public
+        onlyDonator
+        inState(State.Created)
+    {
+        emit Finished();
+        state = State.Inactive;
+    }
+
+    /// 구매자로서 구매를 확정합니다.
+    /// 이 이더는 confirmReceived()가 호출 될때까지
+    /// 잠길것입니다.
+    function donate()
+        public
+        inState(State.Created)
+        payable 
+    {
+        emit Donation();
+        donator = msg.sender;
+        state = State.Locked;
+    }
+
+    /// 구매자가 아이템을 받았다고 확인.
+    /// 이것은 잠긴 이더를 풀어줄것입니다.
+    function confirmReceived()
+        public
+        onlyFoundation
+        inState(State.Locked)
+    {
+        emit DonationReceived();
+        // It is important to change the state first because
+        // otherwise, the contracts called using `send` below
+        // can call in again here.
+        // 먼저 상태를 변경하는 것이 중요합니다.
+        // 그렇지 않으면, `send`를 사용하며 호출된 콘트렉트는
+        // 다시 여기를 호출할 수 있기 때문입니다.
+        state = State.Inactive;
+
+        // NOTE: 이것은 실제로 구매자와 판매자 둘다 환급 하는 것을 막을 수
+        // 있도록 합니다. - 출금 패턴이 사용되어야만 합니다.
+
+        hlogName.transfer(donation_amount);
+    }
+
 }
