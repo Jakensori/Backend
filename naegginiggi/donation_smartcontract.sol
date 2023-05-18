7ddb69085bd1b8a35ccad0d4154b0c73f252c062
이가현 <igh2468@likelion.org>
2023-05-18 15:43:09 +0900

    메타마스크 테스트넷 배포와 리믹스로의 테스트 후, 최종본

--- naegginiggi/donation_smartcontract.sol
+++ naegginiggi/donation_smartcontract.sol
@@ -2,12 +2,6 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract NaeggiDonation {
-    // 초기 비즈니스 로직
-    // safe remote purchase와 비슷
-    // 기부자 측에서 캠페인 모금금고에 입금 시, 트랜젝션 발생 (기부자 포인트 차감 -> 나눔단체 계약서의 balance 증가) - donate 함수
-    // 기부자, 나눔단체 사이에는 포인트로만 거래함
-    // 1. 나눔단체는 캠페인에서 설정한 목표기부금액에 도달했을 때 abort 함수를 통해 출금 가능 (트랜젝션 발생)
-    // 2. 누구든지 언제 누적기부금액 확인 가능 (check 함수)
     address payable public hlogName;
     uint public donationAmount;
     uint256 public current;
@@ -18,7 +12,7 @@ contract NaeggiDonation {
     constructor(address payable _hlogName, uint _donationAmount) payable {
         hlogName = _hlogName;
         donationAmount = _donationAmount;
-        current = current + msg.value;
+        current = 0;
     }
 
     modifier onlyDonator() {
@@ -34,7 +28,7 @@ contract NaeggiDonation {
     function abort()
         public
     {
-        require(address(this).balance <= current, "Donation amount is not reached.");
+        require(address(this).balance <= donationAmount, "Donation amount is not reached.");
         emit Finished();
         payable(hlogName).transfer(address(this).balance);
     }
@@ -49,6 +43,8 @@ contract NaeggiDonation {
         payable 
     {
         emit Donation(msg.value);
+        current = current + msg.value;
+        payable(msg.sender).transfer(msg.value);
     }
 
     receive() external payable {
